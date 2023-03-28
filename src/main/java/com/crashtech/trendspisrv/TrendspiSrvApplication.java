97bac35f16063640a8de71809f0c305365f72bad
Afsar <afsar.s33@gmail.com>
2023-03-28 06:34:15 +0530

    Add files via upload

---
+++ src/main/java/com/crashtech/trendspisrv/TrendspiSrvApplication.java
@@ -0,0 +1,13 @@
+package com.crashtech.trendspisrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendspiSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendspiSrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crashtech/trendspisrv/TrendspiSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crashtech.trendspisrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendspiSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
