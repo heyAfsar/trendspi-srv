ccc0738986476bfc587445866b5303f57e3f23ed
Altaf Raza <52417530+mealtafraza@users.noreply.github.com>
2023-04-14 09:10:16 +0530

    Service to service communication (#6)
    * Post API for Array of Object implemented
    
    * Added PUT and DELETE API and Junit test case
    
    * Added Documentation
    
    * Added internationalization
    
    * delete multiple trends Call orConsume External API

--- src/main/java/com/crashtech/trendspisrv/controller/MainController.java
+++ src/main/java/com/crashtech/trendspisrv/controller/MainController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.crashtech.trendspisrv.modal.Stocks;
 import com.crashtech.trendspisrv.repository.StocksRepository;
@@ -29,6 +31,18 @@ public class MainController {
 	public ResponseEntity<?> getStocks() {
 		return ResponseEntity.ok(this.stocksRepository.findAll());
 	}
+	
+	//Getting single Stock 
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Stocks> getStock(@PathVariable String id) {
+        Optional<Stocks> optionalDocument = stocksRepository.findById(id);
+        if (optionalDocument.isPresent()) {
+        	Stocks document = optionalDocument.get();
+            return ResponseEntity.ok(document);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 	//Posting one Stock
 	@PostMapping("/push/trend")
@@ -65,5 +79,28 @@ public class MainController {
         stocksRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
+	
+	//Deleting multiple stocks
+	@DeleteMapping("/delete/trends")
+    public ResponseEntity<Void> deleteStocks(@RequestBody List<String> ids) {
+        stocksRepository.deleteAllByIdIn(ids);
+        return ResponseEntity.noContent().build();
+    }
 
+	//How to Call or Consume External API in Spring Boot
+	@GetMapping("/get/empmicroservicedata")
+	public ResponseEntity<?> empmicroservicedata() {
+	    try {
+	        String uri="https://dummy.restapiexample.com/api/v1/employees";
+	        RestTemplate restTemplate = new RestTemplate();
+	        String result = restTemplate.getForObject(uri, String.class);
+	        return new ResponseEntity<>(result, HttpStatus.OK);
+	    }catch (Exception e){
+	        e.printStackTrace();
+	        return new ResponseEntity<>("Error!, Please try again", HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	}
+	
+	
+	
 }
