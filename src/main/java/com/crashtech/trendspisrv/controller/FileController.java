58db5061dcba6261e2a81bf917a49491f3e1aeb6
Altaf Raza <52417530+mealtafraza@users.noreply.github.com>
2023-04-15 18:40:34 +0530

    Uploadfile (#7)
    * Post API for Array of Object implemented
    
    * Added PUT and DELETE API and Junit test case
    
    * Added Documentation
    
    * Added internationalization
    
    * delete multiple trends Call orConsume External API
    
    * Added upload and download file API

---
+++ src/main/java/com/crashtech/trendspisrv/controller/FileController.java
@@ -0,0 +1,40 @@
+package com.crashtech.trendspisrv.controller;
+
+import com.crashtech.trendspisrv.modal.LoadFile;
+import com.crashtech.trendspisrv.service.FileService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api")
+public class FileController {
+
+    @Autowired
+    private FileService fileService;
+
+    @PostMapping("/push/files")
+    public ResponseEntity<?> upload(@RequestParam("file")MultipartFile file) throws IOException {
+        return new ResponseEntity<>(fileService.addFile(file), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/file/{id}")
+    public ResponseEntity<ByteArrayResource> download(@PathVariable String id) throws IOException {
+        LoadFile loadFile = fileService.downloadFile(id);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(loadFile.getFileType() ))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + loadFile.getFilename() + "\"")
+                .body(new ByteArrayResource(loadFile.getFile()));
+    }
+
+}
