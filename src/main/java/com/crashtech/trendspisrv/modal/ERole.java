6bc3327e1d19cc0501b6b04a9dbc4d80645eab2d
Altaf <mealtafraza@gmail.com>
2023-04-27 11:13:20 +0530

    Added securit

---
+++ src/main/java/com/crashtech/trendspisrv/modal/ERole.java
@@ -0,0 +1,8 @@
+package com.crashtech.trendspisrv.modal;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
+
---
+++ src/main/java/com/crashtech/trendspisrv/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.crashtech.trendspisrv.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.crashtech.trendspisrv.modal.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
+
